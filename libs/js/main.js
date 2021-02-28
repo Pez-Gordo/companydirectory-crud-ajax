6dd55454b2484fe5729c48e52b675f7ed9213412
pez <emvenegas@gmail.com>
2021-02-28 15:22:37 +0000

    Added reset, search and equals functions

--- libs/js/main.js
+++ libs/js/main.js
@@ -68,3 +68,113 @@ function buildTable() {
         }
     })
 }
+
+function startsWith(db, i, filterBy, searchText) {
+
+    var strLength =  searchText.length;
+
+    if ((db[i][filterBy].toLowerCase()).slice(0, strLength) == searchText.toLowerCase()) {
+        appendEntry(db, i, filterBy)
+        return 1;
+    }
+    return 0;
+}
+
+function endsWith(db, i, filterBy, searchText) {
+
+    var strLength =  searchText.length;
+
+    if ((db[i][filterBy].toLowerCase()).slice(-strLength) == searchText.toLowerCase()) {
+        appendEntry(db, i, filterBy)
+        return 1;
+    }
+    return 0;
+}
+
+function contains(db, i, filterBy, searchText) {
+
+    var searchTextArr = searchText.split(' ')
+
+    for (let idx in searchTextArr) {
+
+        if ((db[i][filterBy].toLowerCase()).indexOf(searchTextArr[idx].toLowerCase()) >= 0) {
+            appendEntry(db, i, filterBy)
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+function equals(db, i, filterBy, searchText) {
+
+    if (db[i][filterBy].toLowerCase() == searchText.toLowerCase()) {
+        appendEntry(db, i, filterBy)
+        return 1;
+    }
+    return 0;
+}
+
+function search() {
+
+    clearTable();
+
+    var filterBy = $('.filterSelect:first').val()
+    var filterQuery = $('.filterSelect:eq(1)').val()
+    var searchText = $('#searchBar').val()
+
+    $.ajax({
+        type: 'GET',
+        url: 'libs/php/getAll.php', 
+        dataType: 'json',
+        success: function(data) {
+
+            var db = data.data;
+
+            var numberOfEntries = 0;
+
+            for (let i in db) {
+
+                switch (filterQuery) {
+                    case "Starts with":
+                        numberOfEntries += startsWith(db, i, filterBy, searchText)
+                        break;
+                    case "Ends with":
+                        numberOfEntries += endsWith(db, i, filterBy, searchText)
+                        break;
+                    case "Contains":
+                        numberOfEntries += contains(db, i, filterBy, searchText)
+                        break;
+                    case "Equals":
+                        numberOfEntries += equals(db, i , filterBy, searchText)
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+
+            $('#numberOfEntries').html(numberOfEntries)
+
+            if ($('#editModeToggle').prop('checked') == true) {
+                editModeOn()
+            }
+            
+            $(`#${filterBy}Header`).removeClass()
+
+        }
+    })
+
+}
+
+function reset() {
+
+    $('.filterSelect:first').val("lastName")
+    $('.filterSelect:eq(1)').val("Starts with")
+    $('#searchBar').val("")
+    $('#editModeToggle').prop('checked', false);
+
+    clearTable()
+    buildTable()
+}
+
